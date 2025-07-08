01e5159195b3d9711c5831e9e6d279b65d409a45
Samar Abbas <samarabbas172003@gmail.com>
2025-07-08 09:45:13 +0530

    claude to geminir

--- client/src/Content/cont.jsx
+++ client/src/Content/cont.jsx
@@ -134,7 +134,7 @@ const FloatingChatButton = () => {
       {!showChat && (
         <button
           onClick={handleClick}
-          className="fixed bottom-4 right-4 bg-black text-white px-4 py-2 rounded-full shadow-lg z-50 hover:bg-gray-800 transition"
+          className="fixed h-max w-[60vw] bottom-4 right-4 bg-black text-white px-4 py-2 rounded-full shadow-lg z-50 hover:bg-gray-800 transition"
         >
           💬 Samar.AI
         </button>
